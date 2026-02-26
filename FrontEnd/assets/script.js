acb79c26e4c7dc1d19652ca6b2e8b51361843b67
CédricC <syman77680@gmail.com>
2026-02-26 16:25:23 +0100

    manque: body jaune+footer // btn modifier // btn valider grisé

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -29,6 +29,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const addPhotoBtn = document.getElementById('add-photo');
         const addPhotoForm = document.getElementById('add-photo-form');
         const backBtn = document.getElementById('back-to-gallery');
+        const separator = document.querySelector('.separator');
 
         // Affiche la galerie
         modalTitle.textContent = 'Galerie photo';
@@ -36,6 +37,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         addPhotoBtn.style.display = 'flex';
         addPhotoForm.style.display = 'none';
         backBtn.style.display = 'none';
+        separator.style.display = 'flex';
         modalGallery.innerHTML = '';
         works.forEach(work => {
             // Crée une figure pour chaque work
@@ -51,7 +53,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
             // Icône corbeille (SVG)
             const trash = document.createElement('span');
-            trash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+            trash.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path fill="rgb(255, 255, 255)" d="M232.7 69.9C237.1 56.8 249.3 48 263.1 48L377 48C390.8 48 403 56.8 407.4 69.9L416 96L512 96C529.7 96 544 110.3 544 128C544 145.7 529.7 160 512 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L224 96L232.7 69.9zM128 208L512 208L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 208zM216 272C202.7 272 192 282.7 192 296L192 488C192 501.3 202.7 512 216 512C229.3 512 240 501.3 240 488L240 296C240 282.7 229.3 272 216 272zM320 272C306.7 272 296 282.7 296 296L296 488C296 501.3 306.7 512 320 512C333.3 512 344 501.3 344 488L344 296C344 282.7 333.3 272 320 272zM424 272C410.7 272 400 282.7 400 296L400 488C400 501.3 410.7 512 424 512C437.3 512 448 501.3 448 488L448 296C448 282.7 437.3 272 424 272z"/></svg>`;
             trash.classList.add('trash-icon');
             // Suppression du work de la liste affichée
             trash.addEventListener('click', async (event) => {
@@ -94,18 +96,21 @@ document.addEventListener('DOMContentLoaded', async () => {
         const addPhotoBtn = document.getElementById('add-photo');
         const addPhotoForm = document.getElementById('add-photo-form');
         const backBtn = document.getElementById('back-to-gallery');
+        const separator = document.querySelector('.separator');
 
         modalTitle.textContent = 'Ajout photo';
         modalGallery.style.display = 'none';
         addPhotoBtn.style.display = 'none';
         addPhotoForm.style.display = 'flex';
         backBtn.style.display = 'flex';
+        separator.style.display = 'none';
     }
 
     // Preview image
     const photoInput = document.getElementById('photo-input');
     const photoPreview = document.getElementById('photo-preview');
     const photoLabel = document.querySelector('.add-photo-label');
+    const fileFormatInfo = document.getElementById('file-format');
     if (photoInput && photoPreview && photoLabel) {
         photoInput.addEventListener('change', function (e) {
             const file = e.target.files[0];
@@ -114,6 +119,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 reader.onload = function (evt) {
                     photoPreview.src = evt.target.result;
                     photoPreview.style.display = 'flex';
+                    fileFormatInfo.style.display = 'none';
                     photoLabel.style.display = 'none';
                 };
                 reader.readAsDataURL(file);
@@ -129,6 +135,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             photoPreview.src = '';
             photoPreview.style.display = 'none';
             photoLabel.style.display = 'flex';
+            fileFormatInfo.style.display = 'block';
         });
     }
 
